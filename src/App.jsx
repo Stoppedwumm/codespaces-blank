044d3aaab584f29c61e8fce67b6a35195f92b341
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2025-12-23 14:38:48 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/electron-vite.animate.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
